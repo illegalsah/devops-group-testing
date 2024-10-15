9110e091a114cadf7b1e7be63a4b3fb9b035e6e5
padmanabha <tech.padmanabha@gmail.com>
2024-10-15 15:32:27 +0530

    initial setup

---
+++ src/main/java/io/pn/dev/DevopsGroupTestingApplication.java
@@ -0,0 +1,13 @@
+package io.pn.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGroupTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGroupTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pn/dev/DevopsGroupTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pn.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGroupTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
